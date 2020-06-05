8b2c31c02699df10e94f9d75abd9becabc5eb967
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2020-06-04 21:44:40 -0500

    Fix api tests

--- spec/api/bike.spec.js
+++ spec/api/bike.spec.js
@@ -1,27 +1,21 @@
 var mongoose = require('mongoose');
-var Bike = require('../../models/bike');
-// var server = require('../../bin/www');
 var axios = require('axios');
-
 var baseUrl = 'http://localhost:3000/api/bikes';
 
 describe('Bike api', () => {
-  beforeEach(function (done) {
-    var mongoDB = 'mongodb://localhost/testdb';
-    mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+  var mongoDB = 'mongodb://localhost/testdbapi';
+  mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error'));
-    db.once('open', function () {
-      console.log('We are connected to a test db');
-      done();
-    });
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error'));
+  db.once('open', function () {
+    console.log('We are connected to a test db for the api');
   });
 
-  afterEach(function (done) {
-    Bike.deleteMany({}, function (err, success) {
-      if (err) console.log(err);
-      done();
+  beforeEach(async function () {
+    const res = await axios.get(baseUrl)
+    res.data.bikes.forEach(async (bike) => {
+      await axios.post(baseUrl + '/delete', { code: bike.code })
     })
   });
 
@@ -32,9 +26,9 @@ describe('Bike api', () => {
         expect(res.data.bikes.length).toBe(0);
 
         var a = {
-          code: 1, 
-          color: 'red', 
-          model: 'urban', 
+          code: 1,
+          color: 'red',
+          model: 'urban',
           lat: -34.6012424,
           lng: -58.3861497
         }
@@ -49,59 +43,59 @@ describe('Bike api', () => {
       })
     });
   })
-})
 
-xdescribe('POST bikes /create', () => {
-  it('Status 200', async () => {
-    var a = {
-      id: 10,
-      color: 'red',
-      model: 'normal',
-      lat: -34.6012424,
-      lng: -58.3861497
-    }
+  describe('POST bikes /create', () => {
+    it('Status 200', async () => {
+      var a = {
+        id: 10,
+        color: 'red',
+        model: 'normal',
+        lat: -34.6012424,
+        lng: -58.3861497
+      }
 
-    const res = await axios.post('http://localhost:3000/api/bikes/create', a)
-    expect(res.status).toBe(200)
+      const res = await axios.post('http://localhost:3000/api/bikes/create', a)
+      expect(res.status).toBe(200)
+    })
   })
-})
 
-xdescribe('POST bikes /delete', () => {
-  it('Deletes a bike', async () => {
-    var a = {
-      id: 10,
-      color: 'red',
-      model: 'normal',
-      lat: -34.6012424,
-      lng: -58.3861497
-    }
+  describe('POST bikes /delete', () => {
+    it('Deletes a bike', async () => {
+      var a = {
+        id: 10,
+        color: 'red',
+        model: 'normal',
+        lat: -34.6012424,
+        lng: -58.3861497
+      }
 
-    await axios.post('http://localhost:3000/api/bikes/create', a)
-    const res = await axios.post('http://localhost:3000/api/bikes/delete', { id: a.id })
-    expect(res.status).toBe(200)
+      await axios.post('http://localhost:3000/api/bikes/create', a)
+      const res = await axios.post('http://localhost:3000/api/bikes/delete', { id: a.id })
+      expect(res.status).toBe(200)
+    })
   })
-})
 
-xdescribe('POST bikes /update', () => {
-  it('Deletes a bike', async () => {
-    var a = {
-      id: 10,
-      color: 'red',
-      model: 'normal',
-      lat: -34.6012424,
-      lng: -58.3861497
-    }
+  describe('POST bikes /update', () => {
+    it('Deletes a bike', async () => {
+      var a = {
+        id: 10,
+        color: 'red',
+        model: 'normal',
+        lat: -34.6012424,
+        lng: -58.3861497
+      }
 
-    var aUpdated = {
-      id: 10,
-      color: 'blue',
-      model: 'super',
-      lat: -64.6012424,
-      lng: -78.3861497
-    }
+      var aUpdated = {
+        id: 10,
+        color: 'blue',
+        model: 'super',
+        lat: -64.6012424,
+        lng: -78.3861497
+      }
 
-    await axios.post('http://localhost:3000/api/bikes/create', a)
-    const res = await axios.post('http://localhost:3000/api/bikes/update', aUpdated)
-    expect(res.status).toBe(200)
+      await axios.post('http://localhost:3000/api/bikes/create', a)
+      const res = await axios.post('http://localhost:3000/api/bikes/update', aUpdated)
+      expect(res.status).toBe(200)
+    })
   })
 })
--- spec/models/bike.spec.js
+++ spec/models/bike.spec.js
@@ -3,22 +3,18 @@ var Bike = require('../../models/bike');
 // var doesntExist = require('../utils').doesntExist;
 
 describe('Bike model', () => {
-  beforeEach(function (done) {
-    var mongoDB = 'mongodb://localhost/testdb';
-    mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+  var mongoDB = 'mongodb://localhost/testdbmodel';
+  mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error'));
-    db.once('open', function () {
-      console.log('We are connected to a test db');
-      done();
-    });
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error'));
+  db.once('open', function () {
+    console.log('We are connected to a test db for the model');
   });
 
-  afterEach(function (done) {
+  beforeEach(function () {
     Bike.deleteMany({}, function (err, success) {
       if (err) console.log(err);
-      done();
     })
   });
 
@@ -77,63 +73,3 @@ describe('Bike model', () => {
     })
   });
 });
-
-// TODO: https://www.coursera.org/learn/desarrollo-lado-servidor-nodejs-express-mongodb/lecture/hT5H6/odm-utilizando-mongoose-en-nuestro-proyecto
-// min 16:20
-
-// Bike.testReset = function () {
-//   Bike.all.forEach(b => {
-//     Bike.removeById(b.id)
-//   })
-// };
-
-// beforeEach(() => {
-//   Bike.testReset()
-// })
-
-// describe('Bike model', () => {
-//   describe('Bike.all', () => {
-//     it('begins empty', () => {
-//       expect(Bike.all.length).toBe(0);
-//     });
-//   });
-
-//   describe('Bike.add', () => {
-//     it('adds one bike', () => {
-//       var a = new Bike(1, 'red', 'urban', [-34.6012424, -58.3861497]);
-
-//       Bike.add(a);
-//       expect(Bike.all.length).toBe(1);
-//       expect(Bike.all[0]).toBe(a);
-//     })
-//   })
-
-//   describe('Bike.findById', () => {
-//     it('returns bike with correct id', () => {
-//       var a = new Bike(1, 'red', 'urban', [-34.6012424, -58.3861497]);
-//       var b = new Bike(2, 'blanca', 'urban', [-32.6012424, -57.3861497]);
-
-//       Bike.add(a);
-//       Bike.add(b);
-
-//       var targetBike = Bike.findById(1);
-//       expect(targetBike.id).toBe(1);
-//       expect(targetBike.color).toBe('red');
-//       expect(targetBike.model).toBe('urban');
-//       expect(targetBike.location).toEqual([-34.6012424, -58.3861497]);
-//     });
-//   })
-
-//   describe('Bike.removeById', () => {
-//     it('removes a bike', async () => {
-//       var a = new Bike(1, 'red', 'urban', [-34.6012424, -58.3861497]);
-//       var b = new Bike(2, 'blanca', 'urban', [-32.6012424, -57.3861497]);
-
-//       Bike.add(a);
-//       Bike.add(b);
-//       Bike.removeById(1);
-//       var notFound = await doesntExist(() => Bike.findById(1));
-//       expect(notFound).toBe(true);
-//     })
-//   })
-// });
